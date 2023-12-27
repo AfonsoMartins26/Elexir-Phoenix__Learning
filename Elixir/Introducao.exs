9d148fa6729dfd387ab6ff4ba82078a3b19f4a3a
Afonso Martins <145707571+AfonsoMartins26@users.noreply.github.com>
2023-12-27 23:57:33 +0000

    mais alguma informacao
    head ,tail ,map ,Enum and lists

--- Elixir/Introducao.exs
+++ Elixir/Introducao.exs
@@ -30,5 +30,24 @@ rem 10,2 = 0"
 #iex> greeting.("Izzy", "Female", "30ish")
 #"Hello, Izzy! I see you're Female and you're 30ish years old!"
 
+# head -> hd
+# tail -> tl
+
+"iex> [1,2,3,4,5] -- [1,2,3]
+[4,5]"
+#iex>map = %{:foo => "bar", "hello" => :world}
+#    %{:foo => "bar", "hello" => :world}
+#iex>map[:foo]
+#    "bar"
+#iex>map["hello"]
+#    :world
+
+
+
+#iex>Enum.all?(["foo", "bar", "hello"], fn(s) -> String.length(s) == 3 end)
+#false
+#iex>Enum.all?(["foo", "bar", "hello"], fn(s) -> String.length(s) > 1 end)
+#true
+
 
 end
