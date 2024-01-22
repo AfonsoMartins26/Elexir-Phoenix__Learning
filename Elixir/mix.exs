788f63ab1a150ff1ccfd8495c7f92c6e659a1f40
Afonso Martins <145707571+AfonsoMartins26@users.noreply.github.com>
2024-01-22 15:45:17 +0000

    documentos de estudo
    criados apos ter feito: mix new teste  
    E assim é criada uma pasta com todos os elementos para criar uma aplicacao

---
+++ Elixir/mix.exs
@@ -0,0 +1,30 @@
+defmodule Teste.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :teste,
+      version: "0.1.0",
+      elixir: "~> 1.15",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {Teste, []},  
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:uuid, "~> 1.1"}  # pack install  from hex.pm depois têm de se fazer non terminal mix deps.get
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
---
+++ Elixir/teste.ex
@@ -0,0 +1,16 @@
+defmodule Teste do
+  use Application
+  alias UUID
+
+
+
+  def start(_type,_args) do 
+    IO.puts(UUID.uuid4())
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+    def hello do
+    :world
+  end
+
+end
